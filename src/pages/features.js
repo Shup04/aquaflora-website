0f9879e5b0f67e94081aa95018b4f41c7961203b
shup04 <bradleyschmidt04@gmail.com>
2024-05-27 16:38:38 -0700

    started features page, has no content

---
+++ src/pages/features.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../components/layout";
+
+
+const Features = () => {
+    return (
+        <Layout>
+            <div className="h-screen w-screen flex justify-center items-center">
+                <h1 className="text-4xl font-bold">Features</h1>
+            </div>
+        </Layout>
+    )
+}
+
+export default Features
