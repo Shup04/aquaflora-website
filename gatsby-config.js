6dd7b4f0a46d98cd4d0f3ad752473099ad66ea31
shup04 <bradleyschmidt04@gmail.com>
2024-05-31 21:58:55 -0700

    .

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Holo - Gatsby Starter`,
-    description: `Holo is a visually striking and highly customizable open source theme built on the powerful Gatsby framework and integrated with the versatile Decap CMS`,
-    siteUrl: "https://holo-theme.netlify.app/",
-    author: `Travis Lord <hi@travislord.xyz>`,
+    title: `Aquaflora`,
+    description: `Aquaflora is an aquarium managment mobile app, that helps manage maintenance reminders, water parameters, tank stocking, and much more!`,
+    siteUrl: "https://www.aquaflora.ca/",
+    author: `Bradley Schmidt`,
   },
   plugins: [
     `gatsby-plugin-image`,
--- src/pages/features.js
+++ src/pages/features.js
@@ -1,23 +1,23 @@
 // featuresPage.js
 import React from 'react';
-import FeatureSection from '../components/featureSection'
 import Layout from '../components/layout';
+import featureImage from '../images/fishPortrait.png'
 
 const featuresData = [
   {
     title: 'Feature One',
     description: 'Description for feature one. Highlight the key aspects and benefits of this feature.',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-4THL3Qr6SfLCdBuAXX3L9FC5DbN8WUVwJw&s',
+    image: featureImage,
   },
   {
     title: 'Feature Two',
     description: 'Description for feature two. Highlight the key aspects and benefits of this feature.',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-4THL3Qr6SfLCdBuAXX3L9FC5DbN8WUVwJw&s',
+    image: featureImage,
   },
   {
     title: 'Feature Three',
     description: 'Description for feature three. Highlight the key aspects and benefits of this feature.',
-    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-4THL3Qr6SfLCdBuAXX3L9FC5DbN8WUVwJw&s',
+    image: featureImage,
   },
 ];
 
@@ -25,35 +25,38 @@ const FeaturesPage = () => {
     return (
       <Layout>
         <header className="bg-gray-100 text-center py-10">
-          <h1 className="text-4xl font-bold">Our Features</h1>
+          <h1 className="text-black text-3xl font-semibold leading-snug sm:text-3xl lg:text-6xl opacity-70">Our Features</h1>
           <p className="mt-4 text-xl">Explore the amazing features that make our product stand out.</p>
         </header>
-        <div className="px-4 py-10">
+        <div className="max-w-6xl mx-auto mt-10">
+
             <div className="flex flex-col md:flex-row items-center mb-10">
-                <div className="md:w-1/2 p-4">
-                    <h2 className="text-3xl font-semibold mb-4">{featuresData[0].title}</h2>
+                <div className="md:max-w-2/3 p-4">
+                    <h2 className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[0].title}</h2>
                     <p className="text-lg">{featuresData[0].description}</p>
                 </div>
-                <div className="md:w-1/2 p-4">
-                    <img src={featuresData[0].image} alt='title' className="w-full h-auto rounded-lg shadow-lg" />
+                <div className="mx-auto md:w-1/3 md:block p-4 hidden">
+                    <img src={featuresData[0].image} alt='title' className="w-full h-auto rounded-lg" />
                 </div>
             </div>
+
             <div className="flex flex-col md:flex-row items-center mb-10">
-                <div className="md:w-1/2 p-4">
-                    <h2 className="text-3xl font-semibold mb-4">{featuresData[1].title}</h2>
+                <div className="mx-auto md:w-1/3 md:block p-4 hidden">
+                    <img src={featuresData[1].image} alt='title' className="w-full h-auto rounded-lg" />
+                </div>
+                <div className="md:max-w-2/3 p-4">
+                    <h2 className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[1].title}</h2>
                     <p className="text-lg">{featuresData[1].description}</p>
                 </div>
-                <div className="md:w-1/2 p-4">
-                    <img src={featuresData[1].image} alt='title' className="w-full h-auto rounded-lg shadow-lg" />
-                </div>
             </div>
+
             <div className="flex flex-col md:flex-row items-center mb-10">
-                <div className="md:w-1/2 p-4">
-                    <h2 className="text-3xl font-semibold mb-4">{featuresData[2].title}</h2>
+                <div className="md:max-w-2/3 p-4">
+                    <h2 className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[2].title}</h2>
                     <p className="text-lg">{featuresData[2].description}</p>
                 </div>
-                <div className="md:w-1/2 p-4">
-                    <img src={featuresData[2].image} alt='title' className="w-full h-auto rounded-lg shadow-lg" />
+                <div className="mx-auto md:w-1/3 md:block p-4 hidden">
+                    <img src={featuresData[2].image} alt='title' className="w-full h-auto rounded-lg" />
                 </div>
             </div>
         </div>
