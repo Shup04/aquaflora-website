879595f5b9bc3f805f47c4c3f435881de1307bc2
shup04 <bradleyschmidt04@gmail.com>
2024-06-01 12:58:51 -0700

    chances p font for features to  font-montserrat

--- src/pages/features.js
+++ src/pages/features.js
@@ -36,14 +36,14 @@ const FeaturesPage = () => {
       <Layout>
         <header className="bg-gray-100 text-center py-10">
           <h1 className="text-black text-3xl font-semibold leading-snug sm:text-3xl lg:text-6xl opacity-70">Our Features</h1>
-          <h1 className="mt-4 text-xl">Aquaflora has many useful features tailored to hobbiest aquarists.</h1>
+          <h1 className="mt-4 font-montserrat text-xl opacity-70">Aquaflora has many useful features tailored to hobbiest aquarists.</h1>
         </header>
         <div className="max-w-6xl mx-auto mt-10">
 
             <div className="flex flex-col md:flex-row items-center mb-10">
                 <div className="w-2/3 p-4">
                     <h2 style={{lineHeight:1.2}} className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[0].title}</h2>
-                    <p className="text-lg opacity-70">{featuresData[0].description}</p>
+                    <p className="text-lg font-montserrat opacity-70">{featuresData[0].description}</p>
                 </div>
                 <div className="mx-auto w-1/3 md:block p-4 hidden">
                     <img src={featuresData[0].image} alt='title' className="w-full h-auto rounded-lg" />
@@ -56,14 +56,14 @@ const FeaturesPage = () => {
                 </div>
                 <div className="w-2/3 p-4">
                     <h2 style={{lineHeight:1.2}} className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[1].title}</h2>
-                    <p className="text-lg opacity-70">{featuresData[1].description}</p>
+                    <p className="text-lg font-montserrat opacity-70">{featuresData[1].description}</p>
                 </div>
             </div>
 
             <div className="flex flex-col md:flex-row items-center mb-10">
                 <div className="w-2/3 p-4">
                     <h2 style={{lineHeight:1.2}} className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[2].title}</h2>
-                    <p className="text-lg opacity-70">{featuresData[2].description}</p>
+                    <p className="text-lg font-montserrat opacity-70">{featuresData[2].description}</p>
                 </div>
                 <div className="mx-auto w-1/3 md:block p-4 hidden">
                     <img src={featuresData[2].image} alt='title' className="w-full h-auto rounded-lg" />
@@ -76,14 +76,14 @@ const FeaturesPage = () => {
                 </div>
                 <div className="w-2/3 p-4">
                     <h2 style={{lineHeight:1.2}} className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[3].title}</h2>
-                    <p className="text-lg opacity-70">{featuresData[3].description}</p>
+                    <p className="text-lg font-montserrat opacity-70">{featuresData[3].description}</p>
                 </div>
             </div>
 
             <div className="flex flex-col md:flex-row items-center mb-10">
                 <div className="w-2/3 p-4">
                     <h2 style={{lineHeight:1.2}} className="text-black text-3xl lg:text-6xl font-bold xxs:text-2xl xs:text-3xl sm:text-5xl lg:text-6xl text-gradient bg-gradient-to-r from-pink to-purple">{featuresData[4].title}</h2>
-                    <p className="text-lg opacity-70">{featuresData[4].description}</p>
+                    <p className="text-lg font-montserrat opacity-70">{featuresData[4].description}</p>
                 </div>
                 <div className="mx-auto w-1/3 md:block p-4 hidden">
                     <img src={featuresData[4].image} alt='title' className="w-full h-auto rounded-lg" />
