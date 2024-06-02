b4e963559fd77daa31a78091ad60750960c1e018
shup04 <bradleyschmidt04@gmail.com>
2024-06-01 17:15:02 -0700

    added real app images to features page

--- src/pages/features.js
+++ src/pages/features.js
@@ -3,31 +3,38 @@ import React from 'react';
 import Layout from '../components/layout';
 import featureImage from '../images/fishPortrait.png'
 
+import fishItem from '../images/AppImages/fishItem.png'
+import fishScreen from '../images/AppImages/fishScreen.png'
+import infoScreen from '../images/AppImages/infoScreen.png'
+import tankScreen1 from '../images/AppImages/tankScreen1.png'
+import tankScreen2 from '../images/AppImages/tankScreen2.png'
+import paramChart from '../images/AppImages/paramChart.png'
+
 const featuresData = [
   {
     title: 'Custom Tank Profiles',
     description: 'The basis of the app is the ability to create a profile for each of your tanks. Each tank can have its own name, size, image, and short description. All of your fish, reminders, and water parameters stay within each seperate tank profile for easier managment.',
-    image: featureImage,
+    image: tankScreen1,
   },
   {
     title: 'Fish Catalogue',
     description: 'A Short description, image, and table of information uploaded from wikipedia for over 600 commonly kept fish species. Each fish listed has a link to its respective wikipedia page for more in depth information. These fish can be added to any of your tank profiles currently only for keeping track, but in the future this will be used to calculate stocking level based on filter, tank size, plant density, etc.',
-    image: featureImage,
+    image: fishScreen,
   },
   {
     title: 'AI Chatbot',
     description: 'The custom built AI chatbot based on google gemini has been trained to specifically answer simple aquarium related questions. Due to the current nature of AI, this bot should only be used for simple questions and not for any sort of diagnosis for your fish. The messages, and responses ONLY communicate between the users device and gemini, Aquaflora does not save user data.\n\nThe chatbot is still in beta and shouldent be used as a final answer, it is very useful for any quick questions about different fish or equipment/techniques.',
-    image: featureImage,
+    image: tankScreen2,
   },
   {
     title: 'Water Parameter Tracking',
     description: 'Users can save measures parameters for nitrate, nitrite, pH, and ammonia. If current parameters are detected to be above the safe range, you will get an alert on the respective tank profile. This is a great tool for checking to see if there was a past nutrient spike in the case that one of your fish seems sick.',
-    image: featureImage,
+    image: paramChart,
   },
   {
     title: 'Information Center',
     description: 'A great place to go for new aquarists to quickly learn about the hobby. The information center has a list of common fish diseases, types of equipment, maintenance strategies, etc. Each of these lists has a short description, in the future, links to extra resources will be included. This is a great place to start for new aquarists to learn about the hobby and what they might be interested in keeping.',
-    image: featureImage,
+    image: infoScreen,
   },
 ];
 
