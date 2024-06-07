e3a5a6d6b90a5c1a6a1b0c82d67f41333b0c8518
shup04 <bradleyschmidt04@gmail.com>
2024-06-07 15:18:23 -0700

    entire homepage complete

--- src/components/Home/featureSection.js
+++ src/components/Home/featureSection.js
@@ -12,15 +12,15 @@ const FeatureSection = () => {
       <div className=" sm:mt-0 max-w-7xl mx-auto lg:px-6 md:px-3">
         <div className="mb-25 justify-between mx-auto max-w-7xl px-4 sm:mt-10 sm:px-6 md:mt-10 lg:mt-10 lg:px-0 xl:mt-10 flex flex-col sm:flex-row gap-3 lg:flex-justify">
           
-          <div className="lg:inset-y-0 lg:right-0 xl:w-1/3 w-2/5 my-0 flex justify-center lg:flex">
+          <div className="lg:inset-y-0 lg:right-0 xl:w-1/3 sm:w-2/5 w-full my-0 flex justify-center lg:flex">
             <img
-              className="rounded-xl w-full lg:h-full h-auto object-contain"
+              className="rounded-xl sm:w-full w-1/2 lg:h-full h-auto object-contain"
               src={HeroImage}
               alt=""
             ></img>
           </div>
 
-          <div className="xl:w-2/3 sm:w-3/5 w-full sm:text-center sm:text-right flex flex-col justify-center">
+          <div className="xl:w-2/3 sm:w-3/5 w-full text-center sm:text-right flex flex-col justify-center">
             <div className="mb-10">
               <h2 style={{marginBottom:0}} className="text-primaryText md:text-3xl text-2xl font-semibold ">Effortless Organization</h2>
               <h3 style={{lineHeight:1.2}} className="text-primaryText lg:text-6xl md:text-4xl sm:text-3xl text-2xl font-bold text-gradient bg-gradient-to-r from-pink to-purple">
@@ -63,7 +63,7 @@ const FeatureSection = () => {
           <div className="xl:w-2/3 sm:w-3/5 w-full text-center sm:text-left flex flex-col justify-center">
             <div className="lg:inset-y-0 lg:right-0 xl:w-1/3 w-full my-0 flex justify-center lg:flex sm:hidden block">
               <img
-                className="rounded-xl w-3/5 lg:h-full h-full sm:hidden block object-contain"
+                className="rounded-xl w-1/2 lg:h-full h-full sm:hidden block object-contain"
                 src={fishScreen}
                 alt=""
               ></img>
