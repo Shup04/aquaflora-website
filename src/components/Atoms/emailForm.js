9489cf848ee50d3fdb1865ce4d90b5a7ea1539e2
shup04 <bradleyschmidt04@gmail.com>
2024-06-09 00:35:21 -0700

    email form linked to formsplree

--- src/components/Atoms/emailForm.js
+++ src/components/Atoms/emailForm.js
@@ -3,11 +3,15 @@ import React from 'react';
 export const EmailForm = () => {
   return (
     <div className="mt-5 sm:mt-8 sm:flex justify-center w-full w-full sm:justify-start">
-      <form className="flex lg:w-3/4 w-full">
+      <form 
+        action="https://formspree.io/f/mrgnnzpg"
+        method="POST"
+        className="flex lg:w-3/4 w-full">
         <input
           type="email"
           placeholder="Enter your email"
           className="flex-grow p-3 bg-transparent border border-purple text-lg text-white rounded-l-md focus:outline-none"
+          name="email"
           required
         />
         <button
