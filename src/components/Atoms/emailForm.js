ebd0cf27712ed9b0ff8c7de6d70407f37405a00e
shup04 <bradleyschmidt04@gmail.com>
2024-06-09 01:14:11 -0700

    finalized site i think

--- src/components/Atoms/emailForm.js
+++ src/components/Atoms/emailForm.js
@@ -10,13 +10,13 @@ export const EmailForm = () => {
         <input
           type="email"
           placeholder="Enter your email"
-          className="flex-grow p-3 bg-transparent border border-purple text-lg text-white rounded-l-md focus:outline-none"
+          className="flex-grow p-3 sm:w-auto w-1/2 bg-transparent border border-purple text-lg text-white rounded-l-md focus:outline-none"
           name="email"
           required
         />
         <button
           type="submit"
-          className="transition-all duration-500ms hover:bg-transparent border border-purple ease-in-out hover:ease-in-out p-3 text-lg font-bold text-white bg-purple rounded-r-md hover:bg-purple-700 text-base font-medium focus:outline-none"
+          className="transition-all duration-500ms hover:bg-transparent border border-purple ease-in-out hover:ease-in-out p-5 text-lg font-bold text-white bg-purple rounded-r-md hover:bg-purple-700 text-base font-medium focus:outline-none"
         >
           Sign Up
         </button>
