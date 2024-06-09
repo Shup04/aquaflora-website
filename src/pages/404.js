751da76485ced78113301d551a5fb0f7bf47c322
shup04 <bradleyschmidt04@gmail.com>
2024-06-09 00:37:42 -0700

    fixed 404 error screen, looks god

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,13 +5,14 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const NotFoundPage = () => (
+  <div className="bg-darkBG min-h-screen w-full flex flex-col">
   <Layout>
     <Seo title="404: Not found"
          description="404: Not found"
     />
     <div className="404-p">
-    <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist...</p>
+    <h1 className="text-white">404: Not Found</h1>
+      <p className="text-white">You just hit a route that doesn&#39;t exist...</p>
       <a href="/"><Button
                 title="Return Home"
                 colorClass="text-white bg-gradient-to-r from-pink to-purple"
@@ -19,6 +20,7 @@ const NotFoundPage = () => (
               ></Button></a>
       </div>
   </Layout>
+  </div>
 )
 
 export default NotFoundPage
