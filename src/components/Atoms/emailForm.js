299c28a361c03d91ddb5489441a6957b18594354
shup04 <bradleyschmidt04@gmail.com>
2024-06-08 17:08:57 -0700

    email form looking better

--- src/components/Atoms/emailForm.js
+++ src/components/Atoms/emailForm.js
@@ -2,26 +2,22 @@ import React from 'react';
 
 export const EmailForm = () => {
   return (
-    <div className="flex flex-col items-center mt-10">
+    <div className="flex flex-col items-center mt-10 w-full">
       <h2 className="text-3xl font-bold text-white mb-6">Subscribe to our Newsletter</h2>
-      <form 
-        action="https://formspree.io/f/mrgnnzpg"
-        method="POST"
-        className="flex flex-col items-center w-full max-w-md">
+      <form className="flex w-full max-w-md ">
         <input
           type="email"
           placeholder="Enter your email"
-          className="w-full p-3 mb-4 text-lg text-black rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="flex-grow p-3 text-lg text-black rounded-l-md focus:outline-none"
           required
         />
         <button
           type="submit"
-          className="w-full p-3 text-lg font-bold text-white bg-purple-600 rounded-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500"
+          className="p-3 text-lg font-bold text-white bg-purple rounded-r-md hover:bg-purple-700 focus:outline-none"
         >
-          Subscribe
+          Sign Up
         </button>
       </form>
     </div>
   );
 };
-
