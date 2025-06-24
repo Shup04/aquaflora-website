c76b5586780a4b418a5d9ed65bdc102092cdcd08
Shup04 <bradleyschmidt04@gmail.com>
2025-06-23 17:06:47 -0700

    fixed styling up a bit

--- src/components/Atoms/MailerLiteForm.js
+++ src/components/Atoms/MailerLiteForm.js
@@ -31,11 +31,11 @@ const MailerLiteForm = () => {
       className="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-22055133"
     >
       <div className="ml-form-embedWrapper embedForm">
-        <div className="ml-form-embedBody ml-form-embedBodyDefault row-form">
+        <div className="ml-form-embedBody ml-form-embedBodyDefault row-form rounded-lg border border-purple">
           <div className="ml-form-embedContent text-primaryText mb-4">
             <h4 className="text-2xl font-semibold">Sign Up</h4>
-            <p className="text-sm">Signup for the AquaFlora mailing list.</p>
-            <p className="text-sm">
+            <p className="text-sm font-semibold">Signup for the AquaFlora mailing list.</p>
+            <p className="text-sm font-semibold">
 
               Please use the email that is linked to your google play account.
               <br />
@@ -54,7 +54,7 @@ const MailerLiteForm = () => {
                     aria-label="email"
                     aria-required="true"
                     type="email"
-                    className="px-2 py-2 font-montserrat rounded-lg bg-inputBG bg-opacity-20 border-transparent flex-1 appearance-none text-white focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent w-full"
+                    className="px-8 py-3 mb-8 font-montserrat rounded-lg bg-inputBG bg-opacity-20 border-transparent flex-1 appearance-none text-white focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent w-full"
                     name="fields[email]"
                     placeholder="Email"
                     autoComplete="email"
